60032bd53a1d8c432e50d6e46dfce75878aeea44
vapaavetehinen <vapaavetehinen@users.noreply.github.com>
2018-02-24 15:11:44 -0600

    using chunks now
    switched data classifier script to use chunk level data

--- generate_ground_truth/generate_ground_truth.py
+++ generate_ground_truth/generate_ground_truth.py
@@ -7,7 +7,7 @@ prunedlist = []
 
 for each in tsv_files:
     vid_id = each.split('.')[0]
-    if "token_output.tsv" in each and vid_id+".classified.tsv" not in classified_files:
+    if "chunk_output.tsv" in each and vid_id+".classified.tsv" not in classified_files:
         prunedlist.append(each)
 #print(prunedlist)
 
@@ -27,18 +27,14 @@ print ("Now classifying: ", tsv_file.name)
 print()
 
 i = 0
-WIND_LEN = 10
 last_was_ad = False
 while i <len(contents):
-    end_window = i+WIND_LEN
     precent_through = int(i/len(contents)*100)
     percent_str = str(precent_through)+"%"
-    if end_window > len(contents):
-        end_window = len(contents)
     print(percent_str,end=' ')
-    for x in range(i,end_window):
-        print(contents[x].split('\t')[1].strip(),end=' ')
-    print()
+
+    #print(contents[i])
+    print(contents[i].split('\t')[1].strip())
     print(percent_str,end=' ')
     is_ad = ""
     if(last_was_ad):
@@ -55,58 +51,16 @@ while i <len(contents):
     if( is_ad == "n"):
         #code to mark as no
         last_was_ad = False
-        for x in range(i,end_window):
-            write_array[x] = contents[x].strip()+' 0'
-        
-        i+=WIND_LEN
+        write_array[i] = contents[i].strip()+' 0'
+        i+=1
     elif(is_ad == 'y'):
         #code to mark as yes
         last_was_ad = True
-        cnt = 0
-        print(percent_str,end=' ')
-        for x in range(i,end_window):
-            print(str(cnt)+":"+contents[x].split('\t')[1].strip(),end = ' ')
-            cnt+=1
-
-        print()
-        print(percent_str,end=' ')
-        yes_range = input("range?(all):")
-        if yes_range == "":
-            yes_range = "0-"+str(end_window-i-1)
-        #validate input
-        try:
-            assert('-' in yes_range)
-            assert(len(yes_range.split('-')) == 2)
-            low_num = yes_range.split('-')[0]
-            high_num = yes_range.split('-')[1]
-        except Exception as e:
-            print("needs format like 0-2 with one hyphen between two digits")
-            continue
-        try:
-            low_num = int(yes_range.split('-')[0])
-            high_num = int(yes_range.split('-')[1])
-            assert(low_num<=high_num)
-        except Exception as e:
-            print("format is lower int followed by higher int")
-            continue
-        try:
-            assert(low_num >=0 and low_num < end_window-i)
-            assert(high_num >=0 and high_num < end_window-i)
-        except AssertionError as e:
-            print("numbers must be in specified range")
-            continue
-
-        print(percent_str,end=' ')
-        for x in range(i,i+low_num):
-            write_array[x] = contents[x].strip()+' 0'
-        for x in range(i+low_num,i+high_num+1):
-           write_array[x] = contents[x].strip()+' 1'
-        for x in range(i+high_num+1,end_window):
-           write_array[x] = contents[x].strip()+' 0'
-        i+=WIND_LEN
+        write_array[i] = contents[i].strip()+' 1'
+        i+=1
     elif(is_ad == 'b'):
         #code to back up one
-        i-=WIND_LEN
+        i-=1
     else:
         #not one of the options
         print("Please enter 'y', 'n', or 'b'")
