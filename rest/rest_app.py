885f66847181478d2cac7fe61476f4f57bd5672b
vapaavetehinen <vapaavetehinen@users.noreply.github.com>
2018-03-01 21:24:11 -0500

    slight refactor
    need to push slight refactor so server can pull it to continue

--- rest/rest_app.py
+++ rest/rest_app.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from flask import request
 from webvtt import WebVTT
+from flask_cors import CORS
 import os
 import json
 import svm_predict
 from time_stuff import to_seconds
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def hello_world():
     ranges = []
     vid_id = request.args.get('id')
+    if vid_id == "" or not isinstance(vid_id,str):
+        return "Pass an arg ya garg!"
     os.system("youtube-dl --write-auto-sub --skip-download https://www.youtube.com/watch?v="+vid_id+" -o "+vid_id)
-
+    print('outputs are happening')
     captions = WebVTT().read(vid_id+'.en.vtt')
     ad_array = ["" for i in range(len(captions))]
 
@@ -26,6 +29,7 @@ def hello_world():
         words = caption.text
         time = to_seconds(caption.start)/last_time
         ad_array[i] = svm_predict.predict(words,time)
+        print(words, str(ad_array[i]))
         i += 1
     end = -1
     for i in range(len(ad_array)):
@@ -48,4 +52,9 @@ def hello_world():
     for each in ranges:
         if each[0] != each[1]:
             times.append((to_seconds(starts[each[0]]),to_seconds(starts[each[1]])))
-    return json.dumps(times)
+    os.remove(vid_id+'.en.vtt')
+    return "\""+json.dumps(times)+"\""
+
+
+app.run('0.0.0.0', port=80)
+#app.run('0.0.0.0', port=8080, ssl_context=('cert.pem', 'key.pem'))
